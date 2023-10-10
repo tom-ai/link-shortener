fe70a9a3826613cd065e5d70193176a7ac359846
Tom Harris <tomharris3@me.com>
2023-10-10 21:06:22 +0100

    setup states

--- src/App.js
+++ src/App.js
@@ -2,20 +2,11 @@ import Form from './components/Form';
 import Form2 from './components/Form2';
 
 export default function App() {
-  const statuses = ['empty', 'typing', 'submitting', 'success', 'error'];
-
   return (
     <main className="container">
       <h1>Link Shortener</h1>
       <p>Enter a URL to shorten it.</p>
-      {statuses.map((status) => {
-        return (
-          <section>
-            <h2>Form: {status}</h2>
-            <Form2 status={status} />
-          </section>
-        );
-      })}
+      <Form2 />
     </main>
   );
 }
--- src/components/Form2.jsx
+++ src/components/Form2.jsx
@@ -1,23 +1,49 @@
-export default function Form2({ status = 'empty' }) {
+import { useState } from 'react';
+
+export default function Form2() {
+  const [url, setUrl] = useState('');
+  const [link, setLink] = useState('');
+  const [error, setError] = useState(null);
+
+  const [status, setStatus] = useState(''); // typing, submitting, success
+
+  function handleChange(e) {
+    e.preventDefault();
+    setStatus('typing');
+    setUrl(e.target.value);
+  }
+
   return (
     <form>
-      <input disabled={status === 'submitting'} type="text" />
-      <button disabled={status === 'empty' || status === 'submitting'}>
+      <input
+        onChange={(e) => handleChange(e)}
+        disabled={status === 'submitting'}
+        type="text"
+      />
+      <button disabled={url.length === 0 || status === 'submitting'}>
         {status === 'submitting' ? 'Working...' : 'Shorten'}
       </button>
-      {status === 'error' && <p>Something went wrong. Try again.</p>}
-      {status === 'success' && (
-        <>
-          <p>Here is your shortened link:</p>
-          <a href="/">https://rel.ink/abc123</a>
-        </>
+      {error !== null ? (
+        <p>Something went wrong. Try again.</p>
+      ) : (
+        status === 'success' && (
+          <>
+            <p>Here is your shortened link:</p>
+            <a href="/">https://rel.ink/abc123</a>
+          </>
+        )
       )}
     </form>
   );
 }
 
 // empty
-// typing - onChange
-// submitting - onSubmit
-// success - shortenedLink
-// error - error
+// typing - human/typing/onChange
+// submitting - human/pressed button/onSubmit
+// success - network/200/shortenedLink
+// error - network/error/403/catch block
+
+// longUrl
+// error
+
+// typing/.length === 0 || submitting || success
