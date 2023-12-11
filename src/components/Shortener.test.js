6ba2f3f5086f2da3b0bd2cb17fbfd89067817133
Tom Harris <tomharris3@me.com>
2023-12-11 15:52:15 +0000

    update test to reflect new change

--- src/components/Shortener.test.js
+++ src/components/Shortener.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable testing-library/no-debugging-utils */
 import '@testing-library/jest-dom';
 import '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Shortener from './Shortener';
 import userEvent from '@testing-library/user-event';
 import 'whatwg-fetch';
@@ -20,7 +20,7 @@ test('clicking "shorten" displays a shortened link', async () => {
     ok: true,
     json: async () =>
       Promise.resolve({
-        id: shortenedLink,
+        link: shortenedLink,
       }),
   });
 
@@ -51,8 +51,67 @@ test('clicking "shorten" displays a shortened link', async () => {
   ).toBeInTheDocument();
 });
 
-test('returns error message if ', () => {
-  
+// test('returns error message if ', () => {
 
+// });
+
+describe('UI tests', () => {
+  it('renders a URL input with corresponding label', () => {
+    render(<Shortener />);
+    const input = screen.getByLabelText('Long URL:');
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute('type', 'url');
+  });
+
+  it('renders a button with the text "Shorten"', () => {
+    render(<Shortener />);
+    const button = screen.getByRole('button', {
+      name: 'Shorten',
+    });
+    expect(button).toBeInTheDocument();
+  });
 });
 
+describe('Shortener interactions', () => {
+  it('should render a disabled button if input is empty', () => {
+    render(<Shortener />);
+    const input = screen.getByRole('textbox', {
+      name: /long url:/i,
+    });
+    fireEvent.change(input, {
+      target: { value: '' },
+    });
+
+    const button = screen.getByRole('button', {
+      name: 'Shorten',
+    });
+
+    expect(button).toHaveProperty('disabled', true);
+  });
+
+  it('should render an enabled button if input has text', () => {
+    render(<Shortener />);
+
+    const input = screen.getByRole('textbox', {
+      name: /long url:/i,
+    });
+    fireEvent.change(input, {
+      target: { value: 'some input' },
+    });
+
+    const button = screen.getByRole('button', {
+      name: 'Shorten',
+    });
+
+    expect(button).toHaveProperty('disabled', false);
+  });
+
+  it('should disable the shorten button when Shorten is submitting', () => {
+    render(<Shortener />);
+    const input = screen.getByLabelText(/long url:/i);
+    fireEvent.change(input, { target: { value: 'www.mytestlink.com' } });
+    const button = screen.getByRole('button', { name: /shorten/i });
+
+    userEvent.click(button, { name: /shorten/i });
+  });
+});
