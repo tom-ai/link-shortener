24760ccb0cf314f7cfdd1609a4e57746ff11fe4a
Tom Harris <tomharris3@me.com>
2023-12-11 16:17:36 +0000

    add padding to title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Shortener from './components/Shortener';
 
 export default function App() {
   return (
-    <main className="container">
+    <main className="container" style={{ paddingTop: '1.5rem' }}>
       <h1>Link Shortener</h1>
       <Shortener />
     </main>
